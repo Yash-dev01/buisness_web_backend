f2cc120a5fb1b2e5bbe4d3712eea9b7f02eeefcd
Yash_dev_01 <yashagarwal0445@gmail.com>
2026-09-20 01:46:37 +0530

    cors fix

--- server.ts
+++ server.ts
@@ -29,7 +29,7 @@ app.use(
     origin: [
       'http://localhost:5173',
       'http://localhost:4173',
-      'https://khilonapointandcyclestoreofficial.netlify.app/'
+      'https://khilonapointandcyclestoreofficial.netlify.app'
     ],
     credentials: true,
   })
