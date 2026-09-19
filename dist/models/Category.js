fc99784d68b93cd9ba4140f12f042957e214595f
Yash_dev_01 <yashagarwal0445@gmail.com>
2026-09-19 23:46:36 +0530

    new main

--- dist/models/Category.js
+++
@@ -1,9 +0,0 @@
-import mongoose, { Schema } from 'mongoose';
-export const CategorySchema = new Schema({
-    name: { type: String, required: true, trim: true },
-    slug: { type: String, required: true, unique: true, index: true },
-    image: { type: String, default: '' },
-}, {
-    timestamps: true,
-});
-export const CategoryModel = mongoose.models.Category || mongoose.model('Category', CategorySchema);
